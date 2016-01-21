4e07a4b3789d2abbe00bb75953e3b1e7569f5b3e
Goom <gautierpiatek@yahoo.fr>
2016-01-21 10:09:05 -0500

    Ajout Catégorie

--- vues/VueDefaut.class.php
+++ vues/VueDefaut.class.php
@@ -1202,7 +1202,7 @@ class VueDefaut
                         $idCategorie = $categorie->getIdCategorie();
                         echo "<td><span class='icon-list'></span>";
                         echo "<td>".$categorie->getNomCategorie()."</td>" ;
-                        echo "<td><a href='index.php?requete=eliminerCategorie&idCategorie=$idCategorie'><span class='icon-erase'></span></a></td>";
+                        echo "<td><a href='index.php?requete=supprimerCategories&idCategorie=$idCategorie'><span class='icon-erase'></span></a></td>";
                         echo "</tr>";
                     }
                 echo "</table>";
@@ -1506,7 +1506,7 @@ class VueDefaut
                                 <li>
                                     <a href="#">CATEGORIES</a>
                                         <ul>
-                                            <li><a href="#">Ajouter</a></li>
+                                            <li><a href="index.php?requete=ajouterUnCategorie">Ajouter</a></li>
                                             <li><a href="index.php?requete=listeModifierCategories">Modifier</a></li>
                                             <li><a href="index.php?requete=listeSupprimerCategories">Supprimer</a></li>
                                         </ul>
@@ -1711,6 +1711,33 @@ class VueDefaut
        echo "</div>";
     }
     
+    /**
+    * Affiche la page pour ajouter un catégorie
+    * @access public
+    * @auteure: Thuy Tien Vo
+    */
+    public function formulaireAjouterCategorie() 
+    {   
+       
+         ?>
+         <div>
+         <h2 id="titre">Ajouter un categorie</h2>
+         <form method="POST"class="formulaire"action="index.php?requete=ajouterUnCategorie&action=ajoutCategorie">
+             <fieldset>
+              Nom catégorie:<br>
+             <input type="text" name="nomCategorie" >
+             <br> <br>
+              Nom catégorie en Anglais:<br>
+             <input  type="text" name="nomCatAng" >
+              <br> <br>    
+             <input type="submit" value="Envoyer" id="button">
+             <fieldset>
+         </form>  
+         </fieldset>
+         </fieldset>
+         </div>        
+         <?php        
+    }
         //private function rechercheOeuvreParCat()
        // {
             
