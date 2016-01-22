1e4a76f716fc0cab020666ed77400626c3a62b72
German Mahecha <gmahecha@gmail.com>
2016-01-21 20:26:58 -0500

    Ajout des Test Unitaires

--- test/gabarit.test.php
+++ test/gabarit.test.php
@@ -15,12 +15,13 @@
 		</div>
 		<div id="contenu">
 		    <?php 
-            //include("../lib/PdoBDD.class.php");
-          
+            /*include("../lib/PdoBDD.class.php");
+            include("../lib/config/mesParametresBD.php");
             include("../modeles/MOeuvres.class.php");
-            //include ("../modeles/MArrondissement.class.php");
-            //include ("../modeles/MArtistes.class.php");
-            //include ("../modeles/Categories.class.php");
+            include ("../modeles/MArrondissement.class.php");
+            include ("../modeles/MArtistes.class.php");
+            include ("../modeles/MUtilisateurs.class.php");
+            include ("../modeles/MCategories.class.php");*/
             
                 
             /**
@@ -40,7 +41,8 @@
             
             
             /**
-            * Test Unitaire pour la funtion recherche des oeuvres par mot clé                 * @author German Mahecha
+            * Test Unitaire pour la funtion recherche des oeuvres par mot clé                 
+            * @author German Mahecha
             * @version 1.0
             * @update 2015-12-27
             * @license Creative Commons BY-NC 3.0 (Licence Creative Commons Attribution - Pas d’utilisation commerciale 3.0 non transposé)
@@ -64,21 +66,87 @@
             * @license http://creativecommons.org/licenses/by-nc/3.0/deed.fr
             * 
             */         
-            $oArtiste = new MArtistes('','','','','','')
+            $oArtiste=new MArtistes('','','','','','');
             echo "<details>";
             echo "<summary>MArtistes::ajoutArtiste()</summary>";
-            var_dump($oArtiste->listeUnOeuvre( 'test','test','test','test'));
-            echo "</details>"; 
+            var_dump($oArtiste->ajoutArtiste('testUnitaire','testUnitaire','testUnitaire','testUnitaire'));
+            echo "</details>";
             
                        
+            /**
+            * Test Unitaire pour ajouter un utilisateur. 
+            * @author German Mahecha
+            * @version 1.0
+            * @update 2016-01-21
+            * @license Creative Commons BY-NC 3.0 (Licence Creative Commons Attribution - Pas d’utilisation commerciale 3.0 non transposé)
+            * @license http://creativecommons.org/licenses/by-nc/3.0/deed.fr
+            * 
+            */   
+                        
+            $oUtilisateur = new MUtilisateurs('', '', '','', '', '');
+            echo "<details>";
+            echo "<summary>MUtilisateurs::ajoutUtilisateur()</summary>";
+            var_dump($oUtilisateur->ajoutUtilisateur("testUtilisateur", 'testUtilisateur',  'testUtilisateur', 2, 'photo.jpg'));
+            echo "</details>";
             
-			//$oArrondisement = new MArrondissement('', '');
+            /**
+            * Test Unitaire pour supprimer un utilisateur. 
+            * @author German Mahecha
+            * @version 1.0
+            * @update 2016-01-21
+            * @license Creative Commons BY-NC 3.0 (Licence Creative Commons Attribution - Pas d’utilisation commerciale 3.0 non transposé)
+            * @license http://creativecommons.org/licenses/by-nc/3.0/deed.fr
+            * 
+            */   
+                        
+            $oUtilisateur = new MUtilisateurs('', '', '','', '', '');
+            echo "<details>";
+            echo "<summary>MUtilisateurs::supprimerUtilisateurs()</summary>";
+            var_dump($oUtilisateur->supprimerUtilisateurs(2));
+            echo "</details>";
+            
+            
+            
+            /**
+            * Test Unitaire pour modifier un utilisateur. 
+            * @author German Mahecha
+            * @version 1.0
+            * @update 2016-01-21
+            * @license Creative Commons BY-NC 3.0 (Licence Creative Commons Attribution - Pas d’utilisation commerciale 3.0 non transposé)
+            * @license http://creativecommons.org/licenses/by-nc/3.0/deed.fr
+            * 
+            */   
+                        
+            $oUtilisateur = new MUtilisateurs('', '', '','', '', '');
+            echo "<details>";
+            echo "<summary>MUtilisateurs::modifierUtilisateur()</summary>";
+            var_dump($oUtilisateur->modifierUtilisateur(3,'TestUni','TestUni','TestUni',20,'photo2.jpg'));
+            echo "</details>";
+            
+            
+            
+            /**
+            * Test Unitaire pour ajouter une Categorie. 
+            * @author German Mahecha
+            * @version 1.0
+            * @update 2016-01-21
+            * @license Creative Commons BY-NC 3.0 (Licence Creative Commons Attribution - Pas d’utilisation commerciale 3.0 non transposé)
+            * @license http://creativecommons.org/licenses/by-nc/3.0/deed.fr
+            * 
+            */   
+                        
+            $oCategorie = new MCategories('','','');
+            echo "<details>";
+            echo "<summary>MUtilisateurs::ajoutCategorie()</summary>";
+            var_dump($oCategorie->ajoutCategorie('TestUni','TestUni'));
+            echo "</details>";
+            
+             
+            //$oArrondisement = new MArrondissement('', '');
 			//$aArrondissements = $oArrondisement::listeArrondissement();
 			//var_dump($oArrondisement);
             
             
-            $oOeuvre = new MOeuvres('', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
-            $aOeuvre = $oOeuvre::ajouterOeuvre();
 			?>
 		</div>
 		<div id="footer">
